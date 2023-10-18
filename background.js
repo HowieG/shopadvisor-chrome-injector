e32c85771cd6bf37322b83497f5813e7b06be982
Howard Gil <howardbgil@gmail.com>
2023-10-18 12:35:37 -0700

    now only loading from specific sites

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
+let allowedSites = [];
+
+// Load config
+fetch('config.json')
+	.then(response => response.json())
+	.then(config => {
+		allowedSites = config.allowedSites;
+	});
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	if (changeInfo.status == 'complete') {
-		chrome.tabs.executeScript(tabId, { file: 'shopadvisor-injector.js' });
+	if (changeInfo.status === 'complete') {
+		const url = new URL(tab.url);
+		if (allowedSites.some(site => url.hostname.endsWith(site))) {
+			chrome.tabs.executeScript(tabId, { file: 'shopadvisor-injector.js' });
+		}
 	}
 });
