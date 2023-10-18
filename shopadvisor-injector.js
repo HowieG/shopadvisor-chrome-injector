82c47474d6ba860ceecb86db381d0b1c441cae67
Howard Gil <howardbgil@gmail.com>
2023-10-18 12:22:53 -0700

    init commit. Adding ShopAdvisor widget on all sites

---
+++ shopadvisor-injector.js
@@ -0,0 +1,3 @@
+var script = document.createElement('script');
+script.src = "https://shopadvisor-embeddable-widget.vercel.app/index.js?t=" + new Date().getTime();
+document.head.appendChild(script);
